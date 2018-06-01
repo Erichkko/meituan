051e0b0f1e7bf5eae8222e547645779606ff9633
erichkko <erichkko@163.com>
2018-06-01 14:04:57 +0800

    1.修改图片显示问题 2.添加关联点击事件

---
+++ src/components/TypeItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="types-item">
+    <img :src="cellData.icon" >
+    <span>{{cellData.title}}</span>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "TypeItem",
+      props: {
+        cellData: Object,
+    },
+      methods:{
+      }
+    }
+</script>
+
+<style scoped>
+  .types-item {
+    background-color: #fff;
+    float: left;
+    width: 25%;
+    padding-top: 14px;
+  }
+  .types-item img {
+    display: block;
+    width: 50px;
+    margin: 0 auto 12px;
+  }
+  .types-item span {
+    display: block;
+    font-size: 14px;
+    line-height: 14px;
+    text-align: center;
+    color: #2f2f2f;
+  }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="page-content">
+
+
+
   <div class="slider">
     <mt-swipe :auto="3000">
       <mt-swipe-item v-for="item in swipeData" :key="item.pic">
@@ -6,17 +10,68 @@
       </mt-swipe-item>
     </mt-swipe>
   </div>
+    <div class="types">
+      <types-item  v-for="(item,index) in types" :cellData='item' :key="index" @click.native="itemClick(index)">
+
+      </types-item>
+
+    </div>
+  </div>
 </template>
 
 <script>
+  import TypeItem from '../components/TypeItem'
     export default {
       name: "Index",
+      components:{
+       'types-item': TypeItem
+      }
+      ,
       data() {
         return {
           swipeData: [
             {pic:require('../assets/images/swipe/1.png')},
             {pic:require('../assets/images/swipe/2.png')}
           ],
+          types:[
+            {
+              icon: require('../assets/images/types/ms.png'),
+              title: '美食'
+            },
+            {
+              icon: require('../assets/images/types/mtcs.png'),
+              title: '美团超市'
+            },
+            {
+              icon: require('../assets/images/types/sxgs.png'),
+              title: '生鲜果蔬'
+            },
+            {
+              icon: require('../assets/images/types/tdyp.png'),
+              title: '甜点饮品'
+            },
+            {
+              icon: require('../assets/images/types/mtzs.png'),
+              title: '美团专送'
+            },
+            {
+              icon: require('../assets/images/types/zcyx.png'),
+              title: '正餐优选'
+            },
+            {
+              icon: require('../assets/images/types/kcxc.png'),
+              title: '快餐小吃'
+            },
+            {
+              icon: require('../assets/images/types/hbps.png'),
+              title: '汉堡披萨'
+            }
+          ]
+        }
+      },
+      methods:{
+        itemClick(index){
+          console.log('index == '+index);
         }
       }
     }
