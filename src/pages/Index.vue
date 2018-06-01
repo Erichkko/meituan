f7d186199dafb97c0851aa60474b91d68554a140
erichkko <erichkko@163.com>
2018-06-01 11:10:29 +0800

    1.添加轮播图

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,27 @@
 <template>
-    <div>
-      index
-    </div>
+  <div class="slider">
+    <mt-swipe :auto="3000">
+      <mt-swipe-item v-for="item in swipeData" :key="item.pic">
+        <img :src="item.pic">
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
 </template>
 
 <script>
     export default {
-        name: "Index"
+      name: "Index",
+      data() {
+        return {
+          swipeData: [
+            {pic:require('../assets/images/swipe/1.png')},
+            {pic:require('../assets/images/swipe/2.png')}
+          ],
+        }
+      }
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "../assets/css/index";
 </style>
